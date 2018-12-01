b13cb6eb0931f58a6d182465e820cf253e62619e
Paul Vidal <skynnet24@gmail.com>
2018-12-01 12:39:22 +0100

    hello works, missing nickname

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,20 +5,22 @@ import java.text.SimpleDateFormat;
 
 public class Main {
   public static void main(String[] args) {
-    String name;
+    //String nickname;
+    String sentence;
 
-   /* if (args.length > 1 && args[0].equals("-p")) {
-      name = args[1];*/
+    /*if (args.length > 1 && args[0].equals("-p")) {
+      nickname = args[1];
+    }*/
       Scanner in = new Scanner(System.in);
       System.out.println("Enter a string");
-      name = in.nextLine();
-      DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+      DateFormat dateFormat = new SimpleDateFormat("'le' dd MMMM 'et il est' hh:mm");
       Date date = new Date();
       while (true) {
-        if (name == "hello") {
-          System.out.println("Salut, nous sommes le" + dateFormat.format(date));
+        sentence = in.nextLine();
+        if (sentence.equals("hello")) {
+          System.out.println("Salut"+/* nickname*/  ", nous sommes " + dateFormat.format(date));
         }
       }
     }
-  
 }
