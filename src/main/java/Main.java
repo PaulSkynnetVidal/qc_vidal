3e3487b657b1e69e0c942352d97c38217e2d0cab
Paul Vidal <skynnet24@gmail.com>
2018-12-01 17:14:08 +0100

    if forest destroyed in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,10 @@ import java.text.DateFormat;
 import java.util.Scanner;
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.util.StringTokenizer;
 
 public class Main {
   public static void main(String[] args) {
-
     if (args.length > 1 && !args[0].equals("-p")) {
     return ;
     }
@@ -13,51 +13,53 @@ public class Main {
     String nickname = args[1];
     Scanner chatInput = new Scanner(System.in);
     String sentence = chatInput.nextLine();
-
-
     DateFormat dateFormat = new SimpleDateFormat("'le' dd MMMM 'et il est' hh:mm");
     Date currentdate = new Date();
-
 
       while (!sentence.equals("++")) {
 
         sentence = chatInput.nextLine();
         System.out.println(nickname +" : " + sentence);
-        if (sentence.equals("@time")) {
-          System.out.println("Nous sommes " + dateFormat.format(currentdate));
-        }
-        else if (sentence.equals("@hello")) {
-          System.out.println("Salut "+ nickname);
-        }
-        else if (sentence.equals("@"))
-        {
-          System.out.println("");
-        }
-        //ParsingArobase(sentence);
+        WhatTimeIsIt(sentence);
+        SayHello(sentence,nickname);
+        ParsingArobase(sentence);
       }
   }
 
 
-  public void WhatTimeIsIt(String sentence){
+  public static void WhatTimeIsIt(String sentence){
     DateFormat dateFormat = new SimpleDateFormat("'le' dd MMMM 'et il est' hh:mm");
     Date currentdate = new Date();
     if (sentence.equals("@time"))
     {
-      System.out.println("Nous sommes " + dateFormat.format(currentdate));
+      System.out.println("[time] :Nous sommes " + dateFormat.format(currentdate));
     }
     else
       return ;
   }
 
-  public String SayHello(String sentence){
+  public static void SayHello(String sentence, String nickname){
     if (sentence.equals("@hello")){
-      System.out.println("Salut "+ nickname);
+      System.out.println("[hello] : Salut "+ nickname);
     }
+    else
+      return;
   }
 
-  public String ParsingArobase(String sentence){
-    char Separator = '@';
-    
+  public static void ParsingArobase(String sentence){
+    String separator = "@";
+    StringTokenizer st = new StringTokenizer( sentence, separator, true );
 
+    while ( st.hasMoreTokens() ) {
+      String token = st.nextToken();
+      if (token.length() == 1 && separator.indexOf(token.charAt(0)) >= 0) {
+        {
+          if(!token.equals("hello")|| !token.equals("time")){
+            System.out.println("[system] Je ne connais pas le chatbot @foo!");
+          }
+        }
+      }
+
+    }
   }
 }
