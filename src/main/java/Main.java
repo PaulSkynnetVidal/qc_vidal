570fd568411bc52b3f8ab865a0df237b9c8f5d76
Paul Vidal <skynnet24@gmail.com>
2018-12-01 15:54:20 +0100

    code cleaning in progress , destroying iforest inc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,28 +5,59 @@ import java.text.SimpleDateFormat;
 
 public class Main {
   public static void main(String[] args) {
-    String nickname;
-    String sentence;
 
     if (args.length > 1 && !args[0].equals("-p")) {
     return ;
     }
-    nickname = args[1];
+
+    String nickname = args[1];
     Scanner chatInput = new Scanner(System.in);
+    String sentence = chatInput.nextLine();
 
 
     DateFormat dateFormat = new SimpleDateFormat("'le' dd MMMM 'et il est' hh:mm");
     Date currentdate = new Date();
 
 
-    sentence = chatInput.nextLine();
       while (!sentence.equals("++")) {
 
         sentence = chatInput.nextLine();
         System.out.println(nickname +" : " + sentence);
-        if (sentence.equals("@hello")) {
-          System.out.println("Salut "+ nickname +  ", nous sommes " + dateFormat.format(currentdate));
+        if (sentence.equals("@time")) {
+          System.out.println("Nous sommes " + dateFormat.format(currentdate));
         }
+        else if (sentence.equals("@hello")) {
+          System.out.println("Salut "+ nickname);
+        }
+        else if (sentence.equals("@"))
+        {
+          System.out.println("");
+        }
+        //ParsingArobase(sentence);
       }
+  }
+
+
+  public void WhatTimeIsIt(String sentence){
+    DateFormat dateFormat = new SimpleDateFormat("'le' dd MMMM 'et il est' hh:mm");
+    Date currentdate = new Date();
+    if (sentence.equals("@time"))
+    {
+      System.out.println("Nous sommes " + dateFormat.format(currentdate));
     }
+    else
+      return ;
+  }
+
+  public String SayHello(String sentence){
+    if (sentence.equals("@hello")){
+      System.out.println("Salut "+ nickname);
+    }
+  }
+
+  public String ParsingArobase(String sentence){
+    char Separator = '@';
+    
+
+  }
 }
